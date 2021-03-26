00246aec9b2b3724a89e846ef6d636e79bfac3d0
unknown <atdd@kenpugh.com>
2021-03-26 00:09:28 -0400

    Simplified UpdateQuality

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -6,17 +6,17 @@ namespace GildedRoseKata
 
 {
 
-        public class GildedRose
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
         {
-            IList<Item> Items;
-            public GildedRose(IList<Item> Items)
-            {
-                this.Items = Items;
-            }
+            this.Items = Items;
+        }
 
-            public void UpdateQuality()
-            {
-                for (var i = 0; i < Items.Count; i++)
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
             {
 
                 Item item = Items[i];
@@ -26,78 +26,57 @@ namespace GildedRoseKata
 
         private static void UpdateQualityForItem(Item item)
         {
+            if (item.Name == "Sulfuras, Hand of Ragnaros")
+                return;
             if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (item.Quality > 0)
-                {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
+                     item.Quality = item.Quality - 1;
             }
             else
             {
-                if (item.Quality < 50)
+                item.Quality = item.Quality + 1;
+
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    item.Quality = item.Quality + 1;
-
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.SellIn < 11)
                     {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
+                        item.Quality = item.Quality + 1;
+                    }
 
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
+                    if (item.SellIn < 6)
+                    {
+                        item.Quality = item.Quality + 1;
                     }
                 }
+
             }
 
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
-            {
                 item.SellIn = item.SellIn - 1;
-            }
-
-            if (item.SellIn < 0)
+           if (item.SellIn < 0)
             {
                 if (item.Name != "Aged Brie")
                 {
                     if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
+                             item.Quality = item.Quality - 1;
                     }
                     else
                     {
-                        item.Quality = item.Quality - item.Quality;
+                        item.Quality = 0;
                     }
                 }
                 else
                 {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
+                    item.Quality = item.Quality + 1;
                 }
             }
+            if (item.Quality < 0)
+                item.Quality = 0;
+            if (item.Quality > 50)
+                item.Quality = 50;
         }
     }
-    }
+}
 
-    
+
 
